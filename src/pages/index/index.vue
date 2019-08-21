fa33fdc3195a804e84aada99f4b7eab983e90581
coolfish <coolpi07@gmail.com>
2019-08-21 15:01:16 +0800

    init task

---
+++ src/pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- <span class="msg">Hello World</span> -->
+    <van-button type="primary" @click="jump">按钮</van-button>
+    <!-- <van-icon name="close"/> -->
+    <!-- <button @click="jump">按钮</button> -->
+  </div>
+</template>
+
+<script>
+import { post } from '@/utils/request'
+export default {
+  methods: {
+    jump () {
+      post('http://www.baidu.com').then(response => {
+        console.log('12345')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .msg {
+    color: red;
+    font-size: 20px;
+    font-weight: bold;
+  }
+</style>
